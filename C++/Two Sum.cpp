df32e690c9bb6925500b242b1a24e1d83d348d07
Cagatay Akkas <cagatayakkas9191@gmail.com>
2023-03-17 16:48:22 +0300

    Two Sum 2

---
+++ C++/Two Sum.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> map;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int difference = target - nums[i];
+
+            if (map.find(difference) != map.end())
+            {
+                return {map[difference], i};
+            }
+            else
+            {
+                map[nums[i]] = i;
+            }
+        }
+
+        return {};
+    }
+};
