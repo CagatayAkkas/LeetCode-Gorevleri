16a12c328069c242983dc8b7ce1729d482997342
Cagatay Akkas <cagatayakkas9191@gmail.com>
2023-03-17 16:50:34 +0300

    Two Sum

---
+++ Python/Two Sum.py
@@ -0,0 +1,17 @@
+#Python 3.0 versionunu seçmeyi unutmayın =)
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+
+        for i in range(len(nums)):
+            difference = target - nums[i]
+
+            if difference in map:
+                return [map[difference], i]
+            else:
+                map[nums[i]] = i
+
+        return []
